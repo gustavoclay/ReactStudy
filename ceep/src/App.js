1bb205389503d7ec4a274ee6384fcba5c99cd628
gustavoclay <gustavoclay@gmail.com>
2020-11-23 16:28:28 -0300

    Continuar curso React 2.

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -6,16 +6,27 @@ import ListaNotas from "./components/ListaNotas";
 
 class App extends Component {
 
-  criarNota(titulo, texto) {
-    console.log(`NOTA => Titulo: "${titulo}" - Texto: "${texto}"`);
+  constructor() {
+    super();
+    this.state = {
+      notas: []
+    };
   }
 
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas
+    };
+    this.setState(novoEstado);
+  }
 
   render() {
     return (
       <section className="conteudo">
-        <CadastroForm criarNota={this.criarNota} />
-        <ListaNotas />
+        <CadastroForm criarNota={this.criarNota.bind(this)} />
+        <ListaNotas notas={this.state.notas} />
       </section>
     );
   }
