8aaba5357766e6c79c8b5697f1b201ebe739075d
gustavoclay <gustavoclay@gmail.com>
2020-11-23 15:43:05 -0300

    Continuar React aula 05 - 03

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import "./assets/App.css";
+import './assets/index.css';
 import CadastroForm from './components/CadastroForm';
 import ListaNotas from "./components/ListaNotas";
 
 class App extends Component {
+
+  criarNota(titulo, texto) {
+    console.log(`NOTA => Titulo: "${titulo}" - Texto: "${texto}"`);
+  }
+
+
   render() {
     return (
-      <section>
-        <CadastroForm />
+      <section className="conteudo">
+        <CadastroForm criarNota={this.criarNota} />
         <ListaNotas />
       </section>
     );
--- ceep/src/components/ListaNotas/ListaNotas.jsx
+++ ceep/src/components/ListaNotas/ListaNotas.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import CardNota from "./CardNota/CardNota";
+import CardNota from "../CardNota/CardNota";
+import "./style.css";
 
 class ListaNotas extends Component {
   render() {
     return (
-      <ul>
+      <ul className="lista-notas">
         {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
           return (
-            <li key={index}>
+            <li className="lista-notas_item" key={index}>
               <CardNota />
             </li>
           );
