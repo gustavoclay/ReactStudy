86934adfd380462bea0e54b3f5d49dc5e703dedb
gustavoclay <gustavoclay@gmail.com>
2021-01-27 18:42:07 -0300

    Terminado curso de react ciclo de vida dos componentes, começar function components

--- ceep/src/components/ListaCategorias/ListaCategorias.jsx
+++ ceep/src/components/ListaCategorias/ListaCategorias.jsx
@@ -5,15 +5,19 @@ export class ListaCategorias extends Component {
   constructor() {
     super();
     this.state = { categorias: [] };
+    this._novasCategorias = this._novasCategorias.bind(this);
   }
 
   componentDidMount() {
-    this.props.categorias.inscrever(this._novasCategorias.bind(this));
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+  componentWillUnmount() {
+    this.props.categorias.desinscrever(this._novasCategorias);
   }
 
   _novasCategorias(categorias) {
     this.setState({ ...this.state, categorias });
-    console.log(categorias);
   }
 
   _handleEventoInput(evento) {
