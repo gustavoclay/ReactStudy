03b1ee94ebefda959931877f49e58752adc8f817
gustavoclay <gustavoclay@gmail.com>
2021-02-10 18:46:52 -0300

    Curso ReactNative

---
+++ ProjectFundamentals/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Primeiro from './src/components/Primeiro';
+
+export default () => (
+  <View>
+    <Primeiro />
+  </View>
+)
+
---
+++ ProjectFundamentals/src/components/Primeiro.js
@@ -0,0 +1,5 @@
+import { from } from 'core-js/fn/array'
+import React from 'react'
+import { Text } from 'react-native'
+
+export default () => <Text>Primeiro!!!</Text>
