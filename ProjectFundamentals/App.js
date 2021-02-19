a816f244ca361712b33e7dc9347cb48f8bcb5fc6
gustavoclay <gustavoclay@gmail.com>
2021-02-19 17:56:02 -0300

    Continuar  - Usando Comunicação Direta e Indireta

--- ProjectFundamentals/App.js
+++ ProjectFundamentals/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import Contador from './src/components/Contador';
-
+import Pai from './src/components/indireta/Pai';
 
 export default () => (
   <SafeAreaView style={style.App}>
-    <Contador />
-    <Contador inicial={0} passo={10} />
+    <Pai></Pai>
   </SafeAreaView>
 )
 
