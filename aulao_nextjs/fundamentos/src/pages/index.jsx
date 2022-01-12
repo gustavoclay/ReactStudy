8fba4b4aaa01353d113a0ae8d9905da9fbe82361
gustavoclay <gustavoclay@gmail.com>
2022-01-11 21:02:55 -0300

    aulao fundamentos react e nextjs continuar 93

---
+++ aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Cabecalho from '../components/Cabecalho';
+import Layout from '../components/Layout';
+
+export default function inicio() {
+  return (
+    <>
+      <Layout>
+      </Layout>
+    </>
+  )
+}
